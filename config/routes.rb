bc012c67628b8005e65b180c8c0c304bf0e90584
deepak <deepak24chopra@gmail.com>
2018-12-16 11:20:56 +0530

    firstCommit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root "pages#home"
+
+  get '/home', to: "pages#home"
+  get '/about', to: "pages#about"
+
+end
