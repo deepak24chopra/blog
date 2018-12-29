9f871a1727b342b198a813e3e9b763b7fd0128e4
deepak <deepak24chopra@gmail.com>
2018-12-29 08:43:54 +0530

    reduce

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+	before_action :set_article, only: [:edit, :update, :show, :destroy]
+
 	def index
 		@articles = Article.all
 	end
@@ -22,15 +24,15 @@ class ArticlesController < ApplicationController
 	end
 
 	def show
-		@article = Article.find(params[:id])
+		
 	end
 
 	def edit
-		@article = Article.find(params[:id])
+		
 	end
 
 	def update
-		@article = Article.find(params[:id])
+		
 		if @article.update(article_params)
 			flash[:notice] = "Article was successfully updated."
 			redirect_to article_path(@article)
@@ -40,13 +42,18 @@ class ArticlesController < ApplicationController
 	end
 
 	def destroy
-		@article = Article.find(params[:id])
+		
 		@article.destroy
 		flash[:notice] = "Article was deleted successfully."
 		redirect_to articles_path
 	end
 
 	private
+
+	def set_article
+		@article = Article.find(params[:id])
+	end
+
 	def article_params
 		params.require(:article).permit(:title,:description)
 	end
