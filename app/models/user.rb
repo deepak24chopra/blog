cca9a03a5d568fb4962cf61c687e6e4e86c45f70
deepak <deepak24chopra@gmail.com>
2018-12-31 12:13:16 +0530

    validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User <  ApplicationRecord
 
+	has_many :articles
+
+	before_save { self.email = email.downcase }
+
 	validates :username, presence: true, length: {minimum: 3, maximum: 25}, uniqueness: {case_sensitive: false}
 
 	VALID_EMAIL_REGEX= /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
