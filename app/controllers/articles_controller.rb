ce95c9d630e067c1d056fbd0dc456042f2b20cee
deepak <deepak24chopra@gmail.com>
2019-01-05 13:44:25 +0530

    users

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,7 +14,7 @@ class ArticlesController < ApplicationController
 		#debugger
 		#render plain: params[:article].inspect
 		@article = Article.new(article_params)
-		@article.user = User.first
+		@article.user_id = session[:user_id]
 		if @article.save
 			flash[:success] = "Article was successfully created."
 			redirect_to article_path(@article)
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
 
   get '/home', to: "pages#home"
   get '/about', to: "pages#about"
+  get '/login', to: "sessions#new"
+  post '/login', to: "sessions#create"
+  delete '/logout', to: "sessions#destroy"
   resources :articles
 
   get '/signup', to: "users#new"
