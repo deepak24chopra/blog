5b7f14c5ceb930d3d8a627def8051fe3a1ea3439
deepak <deepak24chopra@gmail.com>
2019-01-06 10:19:27 +0530

    third

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,7 @@ class ArticlesController < ApplicationController
 		#debugger
 		#render plain: params[:article].inspect
 		@article = Article.new(article_params)
-		@article.user_id = session[:user_id]
+		@article.user_id = current_user
 		if @article.save
 			flash[:success] = "Article was successfully created."
 			redirect_to article_path(@article)
