676f60d9ac0581d45795619ee6986540e023a784
deepak <deepak24chopra@gmail.com>
2019-01-13 07:48:05 +0530

    category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_12_113153) do
+ActiveRecord::Schema.define(version: 2019_01_13_020820) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
